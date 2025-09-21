f8459e6628f97565818a999cd13632344f985444
RomannaBidnyk <romanna.bidnyk@gmail.com>
2025-09-20 22:35:59 -0400

    initial setup

---
+++ src/main/java/com/contact/contact/ContactBookApplication.java
@@ -0,0 +1,14 @@
+package com.contact.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
